464d5e3935592a0ad458c0ca561a584b5de4b4a8
Girik Bajaj <bajajgirik2010@gmail.com>
2023-01-16 22:06:40 +0530

    Add show and hide password functionality + Refactor code
    - Add additional css helper classes and variables
    - Add constants file
    - Improve AuthForm basic css

--- src/components/authentication/index.tsx
+++ src/components/authentication/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Strings } from "../../constants";
 import styles from "../../styles/authForm.module.css";
 import { UserSigninRequest, UserSignupRequest } from "../../types/api";
 
@@ -24,47 +25,93 @@ const AuthForm = ({ signup }: Props) => {
 
   return (
     <div className={styles.container}>
-      <h2>{signup ? "Sign Up" : "Sign In"}</h2>
+      <h2 className={`txt-cen ${styles.heading}`}>
+        {signup ? Strings.authPage.signUp : Strings.authPage.signIn}
+      </h2>
       <form onSubmit={handleSubmit} className="flex-col gap-small">
         {signup && (
           <>
             <input
               name="firstName"
+              placeholder={Strings.authPage.inputPlaceholders.firstName}
+              className={styles.input}
               type="text"
               value={userSignUpInfo.firstName}
               onChange={handleChange}
+              required
             />
             <input
               name="lastName"
+              placeholder={Strings.authPage.inputPlaceholders.lastName}
+              className={styles.input}
               type="text"
               value={userSignUpInfo.lastName}
               onChange={handleChange}
             />
           </>
         )}
-        <input name="email" type="email" value={user.email} onChange={handleChange} />
-        <input name="password" type="password" value={user.password} onChange={handleChange} />
+        <input
+          name="email"
+          placeholder={Strings.authPage.inputPlaceholders.email}
+          className={styles.input}
+          type="email"
+          value={user.email}
+          onChange={handleChange}
+          required
+        />
+        <PasswordInputField
+          name="password"
+          placeholder={Strings.authPage.inputPlaceholders.password}
+          value={user.password}
+          onChange={handleChange}
+        />
         {signup && (
-          <input
+          <PasswordInputField
             name="confirmPassword"
-            type="password"
+            placeholder={Strings.authPage.inputPlaceholders.confirmPassword}
             value={userSignUpInfo.confirmPassword}
             onChange={handleChange}
           />
         )}
-        <button type="submit">{signup ? "Sign Up" : "Sign In"}</button>
+        <button type="submit" className={styles.btn_submit}>
+          {signup ? "Sign Up" : "Sign In"}
+        </button>
       </form>
       {signup ? (
         <p>
-          Existing User? <Link to="/signin">Sign In</Link>
+          {Strings.authPage.existingUser} <Link to="/signin">{Strings.authPage.signIn}</Link>
         </p>
       ) : (
         <p>
-          New User? <Link to="/signup">Sign Up</Link>
+          {Strings.authPage.newUser} <Link to="/signup">{Strings.authPage.signUp}</Link>
         </p>
       )}
     </div>
   );
 };
 
+type PasswordInputFieldProps = {
+  name: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const PasswordInputField = ({ name, placeholder, value, onChange }: PasswordInputFieldProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => setShowPassword(prev => !prev);
+  return (
+    <div className={`flex gap-small ${styles.password_container}`}>
+      <input
+        name={name}
+        placeholder={placeholder}
+        type={showPassword ? "text" : "password"}
+        value={value}
+        onChange={onChange}
+        required
+      />
+      <button onClick={toggleShowPassword}>🫣</button>
+    </div>
+  );
+};
 export default AuthForm;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,25 @@
 export const LOCAL_STORAGE_KEY = "Oip_data";
+
+export const Strings = {
+  authPage: {
+    inputPlaceholders: {
+      firstName: "First Name",
+      lastName: "Last Name",
+      email: "Email",
+      password: "Password",
+      confirmPassword: "Confirm Password"
+    },
+    signUp: "Sign Up",
+    signIn: "Sign In",
+    newUser: "New User",
+    existingUser: "Existing User?"
+  }
+};
+
+export const ErrorMessages = {
+  invalidEmail: "Please enter a valid email",
+  invalidPassword:
+    "Password must contain uppercase, number and should be greater than 8 characters",
+  passwordMismatch: "Passwords don't match",
+  emptyField: "Field cannot be empty"
+};
